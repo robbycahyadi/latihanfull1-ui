e1674be24aa6d50431e89d7257e828072ec12985
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-25 01:19:06 +0700

    create a category of device

--- src/app/views/master/category-device/new-category-device/new-category-device.component.ts
+++ src/app/views/master/category-device/new-category-device/new-category-device.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {CategoryDeviceService} from '../category-device.service';
+import {CategoryDevice} from '../../../../entity/category-device.model';
 
 @Component({
   selector: 'app-new-category-device',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCategoryDeviceComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
 
-  ngOnInit() {
+  constructor(
+    private _router: Router,
+    private _service: CategoryDeviceService,
+    private _toastr: ToastrService,
+    private _formBuilder: FormBuilder) {
   }
 
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      'name': this._formBuilder.control(''),
+      'description': this._formBuilder.control('')
+    });
+  }
+
+  send(data): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      this._toastr.warning('Field Not Allowed Empty', 'Can\'t Save Change Type',
+        {
+          timeOut: 5000
+        });
+      return;
+    }
+
+    const value: CategoryDevice = this.form.value;
+    this._service.save(value).subscribe(resp => {
+      this._toastr.info('New Category of Device Saved', 'Save Success');
+      this._router.navigate(['master', 'category-device']);
+    }, error => {
+      this._toastr.warning('There is something error', 'Oopss....');
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
 }
